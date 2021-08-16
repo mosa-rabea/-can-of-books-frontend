5095e85a054221ddf07a17d372e8d2b70889b45b
mosa-rabea <mosa@protonmail.com>
2021-08-16 04:00:21 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { withAuth0 } from '@auth0/auth0-react';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
+import Logout from './Logout';
+import Login from './Login';
 
 class Header extends React.Component {
   render() {
@@ -12,9 +15,10 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {this.props.auth0.isAuthenticated ? <Logout/> :<Login/>}
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
